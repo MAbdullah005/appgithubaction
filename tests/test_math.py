e29e6312edb858c387e735c09c42a2f9c2647534
Abdullah Ali <abdullahaliofc@gmail.com>
2025-07-10 13:14:27 -0700

    Update test_math.py

--- tests/test_math.py
+++ tests/test_math.py
@@ -5,6 +5,6 @@ def test_add():
     assert add(9,0)==9
     assert add(8,7)==15
 def test_sub():
-    assert sub(1,2)==1
+    assert sub(1,2)==-1
     assert sub(9,8)==1
-    assert sub(6,2)==-4
+    assert sub(6,2)==4
