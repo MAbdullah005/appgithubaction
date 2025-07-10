8d9088c9097fa8856a7d1e6eac98fe9727e8e1aa
MAbdullah005 <abdullahaliofc@gmail.com>
2025-07-10 13:25:12 -0700

    test test cases

--- tests/test_math.py
+++ tests/test_math.py
@@ -1,4 +1,4 @@
-from src.math import add,sub
+from src.math import add,sub,mul
 
 def test_add():
     assert add(1,2)==3
@@ -8,3 +8,7 @@ def test_sub():
     assert sub(1,2)==-1
     assert sub(9,8)==1
     assert sub(6,2)==4
+def test_mul():
+    assert mul(2,2)==4
+    assert mul(4,5)==20
+    assert mul(0,9)==0
