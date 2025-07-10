c6999a366be6546d07f4184af0fff4884866c8c9
MAbdullah005 <abdullahaliofc@gmail.com>
2025-07-10 08:12:47 -0700

    Unit test cases updated

---
+++ tests/test_math.py
@@ -0,0 +1,10 @@
+from src.math import add,sub
+
+def test_add():
+    assert add(1,2)==3
+    assert add(9,0)==9
+    assert add(8,7)==15
+def test_sub():
+    assert sub(1,2)==1
+    assert sub(9,8)==1
+    assert sub(6,2)==-4
